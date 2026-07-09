486468d8caf6cf0a68a9d6ba01118d60b6cac5f8
habul17 <abulhassanca.dev@gmail.com>
2026-07-09 18:17:57 +0530

    create route

--- index.js
+++ index.js
@@ -7,4 +7,20 @@ const app = express();
 app.use(express.json());
 
 
+app.post("/signup", (req, res) => {
+
+})
+
+app.post("/signin", (req, res) => {
+
+})
+
+app.post("/todo", (req, res) => {
+
+})
+
+app.get("/todo", (req, res) => {
+
+})
+
 app.listen(3000);
